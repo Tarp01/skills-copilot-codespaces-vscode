bf713ff2ac80ae862286fb41a5c3b34fbcb562f6
John Tarpley <39133013+Tarp01@users.noreply.github.com>
2023-10-18 18:42:39 +0000

    Copilot third commit!

--- comments.js
+++ comments.js
@@ -1,37 +1,65 @@
-// Create a web server that can respond to requests for /comments.json with a JSON-encoded array of comments,
-// much like your server did in the previous homework.
+// Create a web server that can respond to requests for /comments.json
+// with a JSON-encoded array of comments, much like your server that
+// responds to /guestbook.json
+//
+// You'll need to read the comments from a file named comments.json
+// and parse them to an array, then respond with the array encoded as
+// JSON.
+//
+// You can test your server by running:
+//   curl -is http://localhost:8080/comments.json
+//
+// When you're done, run learnyouhtml verify comments.js and then
+// run learnyouhtml run comments.js to try out your server.
+//
+// ## HINTS
+//
+// You'll need to use the fs module to read the comments from the
+// file. Here's an example of how to use the fs module to read a file:
+//
+//   var fs = require('fs')
+//   fs.readFile('guestbook.json', function (err, data) {
+//     if (err) throw err
+//     console.log(data.toString())
+//   })
+//
+// You'll also need to use the http module to create the web server.
+// Here's an example of how to create an HTTP server:
+//
+//   var http = require('http')
+//   var server = http.createServer(function (req, res) {
+//     res.end('hello!')
+//   })
+//   server.listen(8080)
+//
+// Finally, you'll need to use JSON.stringify() to convert your
+// JavaScript array to JSON. Here's an example:
+//
+//   var array = ['hello', 'goodbye']
+//   var json = JSON.stringify(array)
+//   console.log(json)
+//
+// In this example, the string that will be printed is:
+//
+//   '["hello","goodbye"]'
+//
+// ## NOTE
+//
+// If you get an error like this when you run your program:
+//
+//   SyntaxError: Unexpected token u
+//
+// It means that you're trying to parse a file that doesn't exist.
+// Make sure you're reading from the correct file!
 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var comments = require('./comments.json');
+var fs = require('fs')
+var http = require('http')
 
-var server = http.createServer(function(req, res) {
-  var path = req.url;
-  var parsed = url.parse(path, true);
-  var query = parsed.query;
-  var pathname = parsed.pathname;
-  if (pathname === '/comments.json') {
-    if (query.search) {
-      var search = query.search.toLowerCase();
-      var searchResults = [];
-      for (var i = 0; i < comments.length; i++) {
-        var searchObj = comments[i];
-        if (searchObj['body'].toLowerCase().indexOf(search) !== -1) {
-          searchResults.push(searchObj);
-        }
-      }
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(searchResults));
-    } else {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(comments));
-    }
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('404 not found');
-  }
-});
+var server = http.createServer(function (req, res) {
+  fs.readFile('comments.json', function (err, data) {
+    if (err) throw err
+    res.end(data.toString())
+  })
+})
 
-server.listen(8080);
-console.log('server listening on port 8080');
+server.listen(8080)
